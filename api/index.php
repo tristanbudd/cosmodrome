9b55d5512cbb2085b34d6778ea7c33e3bda3dec5
Tristan <tristanbudd@outlook.com>
2025-01-20 17:58:28 +0000

    Fixing File Locations

--- api/index.php
+++ api/index.php
@@ -29,7 +29,7 @@ $_SESSION["page_title"] = get_lang("home", $page_language);
         <!-- End Main Content -->
 
         <!-- Importing JavaScript -->
-        <script type="text/javascript" rel="javascript" src="../public/resource/js/global.js"></script>
+        <script type="text/javascript" rel="javascript" src="../public/js/global.js"></script>
         <!-- End Importing JavaScript -->
     </body>
 </html>
--- api/signup.php
+++ api/signup.php
@@ -21,7 +21,7 @@ $_SESSION["page_title"] = get_lang("signup", $page_language);
         <!-- End Main Content -->
 
         <!-- Importing JavaScript -->
-        <script type="text/javascript" rel="javascript" src="../public/resource/js/global.js"></script>
+        <script type="text/javascript" rel="javascript" src="../public/js/global.js"></script>
         <!-- End Importing JavaScript -->
     </body>
 </html>
