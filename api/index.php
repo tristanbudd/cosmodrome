e3a5ec93791ca0b3c44aebf9b8f46e54cb68fa98
Tristan <tristanbudd@outlook.com>
2025-01-20 16:38:08 +0000

    Redirecting to actual index.php

--- api/index.php
+++ api/index.php
@@ -1,28 +1,3 @@
 <?php
-session_start();
-
-include("resource/base_functions.php");
-
-$page_language = get_page_language();
-
-$_SESSION["page_name"] = "home";
-$_SESSION["page_title"] = get_lang("home", $page_language);
-?>
-
-<!DOCTYPE html>
-<html lang="<?php echo($page_language) ?>">
-
-<!-- Importing Page Header & SEO -->
-<?php include("resource/head.php"); ?>
-<!-- End Importing Page Header & SEO -->
-
-<body>
-<!-- Main Content -->
-<p>Shouldn't Be Here</p>
-<!-- End Main Content -->
-
-<!-- Importing JavaScript -->
-<script type="text/javascript" rel="javascript" src="resource/js/global.js"></script>
-<!-- End Importing JavaScript -->
-</body>
-</html>
+header("Location: /index.php");
+exit();
